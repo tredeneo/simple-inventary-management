d22cae70153fe3dc60388e9b554cf693516fbdad
tredeneo <tredeneo@hotmail.com>
2024-02-18 22:40:10 -0300

    adicionado pagina de usuarios

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,50 @@
 slint::include_modules!();
-use slint::{StandardListViewItem, VecModel};
+use slint::{Model, SharedString, StandardListViewItem, VecModel};
 use sqlx::{sqlite::SqlitePool, Row};
 use std::rc::Rc;
 
-async fn pegar_computador() -> anyhow::Result<Vec<String>> {
+#[derive(sqlx::FromRow, Debug, Default)]
+struct DbUser {
+    name: String,
+    login: String,
+    setor: String,
+}
+async fn pegar_computador() -> anyhow::Result<Vec<DbUser>> {
     let pool = SqlitePool::connect("banco.sqlite3").await?;
-    let recs = sqlx::query(
+    let recs = sqlx::query_as::<_, DbUser>(
         r#"
-            select * from 'computer'
+            select name,login,setores.setor from users
+            join setores on setores.id  = users.setor 
+
         "#,
     )
     .fetch_all(&pool)
     .await?;
-    let mut itens = Vec::new();
-    for rec in recs {
-        let model = rec.try_get("model").unwrap_or_default();
-        itens.push(model);
-    }
-    Ok(itens)
+    // dbg!(recs);
+    // let mut itens = Vec::new();
+    // for rec in recs {
+    //     let model = rec.try_get("name").unwrap_or_default();
+    //     itens.push(model);
+    // }
+    // Ok(itens)
+    Ok(recs)
 }
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let myapp = App::new().unwrap();
-    dbg!(pegar_computador().await?);
-    // unreachable!();
-    // let row_data: Rc<VecModel<slint::ModelRc<StandardListViewItem>>> = Rc::new(VecModel::default());
-    // let tmp = pegar_computador().await?.unwrap();
-    // for i in tmp {
-    //     let items = Rc::new(VecModel::default());
-    //     items.push(slint::format!("{0}", i.name));
-    //     row_data.push(items.into());
-    // }
-    // myapp
-    //     .global::<Users>()
-    //     .set_row_data(row_data.clone().into());
+    let row_data: Rc<VecModel<slint::ModelRc<StandardListViewItem>>> = Rc::new(VecModel::default());
+    let tmp = pegar_computador().await?;
+    for i in tmp {
+        let items = Rc::new(VecModel::default());
+        items.push(slint::format!("{0}", i.name).into());
+        items.push(slint::format!("{}", i.login).into());
+        items.push(slint::format!("{}", i.setor).into());
+        row_data.push(items.into());
+    }
+    myapp
+        .global::<Users>()
+        .set_row_data(row_data.clone().into());
 
-    // myapp.run().unwrap();
+    myapp.run().unwrap();
     Ok(())
 }
