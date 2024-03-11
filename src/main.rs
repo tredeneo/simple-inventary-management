3e6bf7dcd98de792638b2fc59a8ea6c8687827ca
tredeneo <tredeneo@hotmail.com>
2024-03-10 23:40:15 -0300

    corrigidos errados estruturais no banco

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ struct DbUser {
     name: String,
     login: String,
     email: String,
+    id: i32,
 }
 
 #[derive(sqlx::FromRow, Debug, Default, Clone)]
@@ -26,14 +27,14 @@ async fn get_users() -> anyhow::Result<Vec<DbUser>> {
     let pool = SqlitePool::connect("banco.sqlite3").await?;
     let recs = sqlx::query_as::<_, DbUser>(
         r#"
-            select name,login,email --setores.setor 
+            select name,login,email,id
             from users
-            join setores on setores.id  = users.setor 
 
         "#,
     )
     .fetch_all(&pool)
     .await?;
+
     Ok(recs)
 }
 
@@ -59,8 +60,8 @@ async fn get_computers(db: &actual_database) -> anyhow::Result<Vec<DbComputer>> 
             select  serialnumber ,brands.name as brand, model, login as actual_user
             from computer 
             join brands on computer.brand  = brands.id 
-            join has on has.computer_id = computer.id 
-            join users on users.id = has.user_id         
+            left join has on has.computer_id = computer.id 
+            left join users on users.id = has.user_id         
         "#,
     )
     .fetch_all(db)
@@ -86,16 +87,23 @@ async fn update_user(user: DbUser) -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn ui_user_list(app: &App) -> anyhow::Result<()> {
-    let row_data: Rc<VecModel<slint::ModelRc<StandardListViewItem>>> = Rc::new(VecModel::default());
+async fn get_user_list() -> anyhow::Result<Rc<VecModel<slint::ModelRc<StandardListViewItem>>>> {
+    let row_data = Rc::new(VecModel::default());
     let tmp = get_users().await?;
     for i in tmp {
         let items = Rc::new(VecModel::default());
         items.push(slint::format!("{0}", i.name).into());
         items.push(slint::format!("{}", i.login).into());
         items.push(slint::format!("{}", i.email).into());
+        items.push(slint::format!("{}", i.id).into());
         row_data.push(items.into());
     }
+    Ok(row_data)
+}
+
+async fn ui_user_list(app: &App) -> anyhow::Result<()> {
+    let row_data = get_user_list().await?;
+    // dbg!(&row_data);
 
     app.global::<Users>().set_row_data(row_data.clone().into());
     Ok(())
@@ -110,6 +118,7 @@ async fn ui_user_detail_update(app: &App) {
             name: detail.get_name().to_string(),
             login: detail.get_login().to_string(),
             email: detail.get_email().to_string(),
+            id: detail.get_id(),
         };
         let tmp = user.clone();
         let _ = slint::spawn_local(async move {
@@ -143,46 +152,76 @@ struct DbHas {
     end: String,
 }
 
-async fn update_user_equipament(user: String, equipament: String) -> anyhow::Result<()> {
+async fn update_user_equipament(
+    actual_user: String,
+    future_user: String,
+    equipament: String,
+) -> anyhow::Result<()> {
     let poll = SqlitePool::connect("banco.sqlite3").await?;
     let today = chrono::Local::now().date_naive().to_string();
-    dbg!(&user);
-    unimplemented!();
+    dbg!(&equipament, &actual_user, &future_user, &today);
     let recs = sqlx::query(
         r#"
-            select * from has -- update has
-            -- set data_fim=?1 
-            where computer_id=?2 and user_id=?3 and data_fim=NULL
+            update has
+            set date_end=?1 
+            where computer_id=(    
+                        select id 
+                    	from users u 
+                    	WHERE u.login =?2        
+                        )
+            and user_id=(
+                     	SELECT id 
+                    	from computer c 
+                    	WHERE c.serialnumber = ?3
+                        )
+            and date_end is NULL
         "#,
     )
     .bind(today)
+    .bind(actual_user)
     .bind(equipament)
-    .bind(user)
     .execute(&poll)
     .await?;
-    // .rows_affected();
+
     dbg!(recs);
+    // let recs = sqlx::query(
+    //     r#"
+
+    //     "#,
+    // )
+    // .execute(&poll)
+    // .await?;
+
+    // .rows_affected();
     Ok(())
 }
-async fn ui_change_equipament(app: &App) {
+async fn ui_change_equipament(app: &App) -> anyhow::Result<()> {
     let myapp = app.clone_strong();
+
+    let row_data = get_user_list().await?;
+    app.global::<ChangeEquipament>()
+        .set_users(row_data.clone().into());
+
     app.global::<ChangeEquipament>().on_change_user(move || {
         let local_app = myapp.clone_strong();
         // let user = myapp.global::<UserDetail>().get_login();
         let computer = myapp.global::<ComputerDetail>();
         let serial = computer.get_serial_number();
 
-        let brand = computer.get_brand();
-        // let login = computer.get_actual_user();
+        // let brand = computer.get_brand();
+        let actual_user = computer.get_actual_user();
         let future_user = local_app.global::<ChangeEquipament>().get_future_user();
-        // let model = computer.get_model();
 
-        // dbg!(&brand, &login, &model, &serial);
         let _ = slint::spawn_local(async move {
-            let _ = update_user_equipament(future_user.to_string(), serial.to_string()).await;
+            let _ = update_user_equipament(
+                actual_user.to_string(),
+                future_user.to_string(),
+                serial.to_string(),
+            )
+            .await;
         });
-        // dbg!(today);
-    })
+    });
+    Ok(())
 }
 
 #[tokio::main]
@@ -193,6 +232,11 @@ async fn main() -> anyhow::Result<()> {
     let _ = ui_user_detail_update(&myapp).await;
     let _ = ui_change_equipament(&myapp).await;
     let _ = ui_equipament_list(&myapp, &poll).await;
+    //     Ok(_) => {}
+    //     Err(e) => {
+    //         println!("{}", e)
+    //     }
+    // };
 
     myapp.run().unwrap();
     Ok(())
